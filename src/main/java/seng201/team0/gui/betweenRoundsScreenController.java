a31459a08ccd42ae0033d3ea22a43758368b9606
Finn <fbr42@uclive.ac.nz>
2024-05-09 14:47:43 +1200

    between round screen and its controller

---
+++ src/main/java/seng201/team0/gui/betweenRoundsScreenController.java
@@ -0,0 +1,48 @@
+package seng201.team0.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class betweenRoundsScreenController {
+
+    @FXML
+    private Label betweenRoundsTitleLabel;
+
+    @FXML
+    private Label currTowerType;
+
+    @FXML
+    private Label currTowerXP;
+
+    @FXML
+    private Button nextRoundButton;
+
+    @FXML
+    private Button selectedTowerSellButton;
+
+    @FXML
+    private Button selectedTowerUpgradeButton;
+
+    @FXML
+    private Button towerButton1;
+
+    @FXML
+    private Button towerButton2;
+
+    @FXML
+    private Button towerButton3;
+
+    @FXML
+    private Button towerButton4;
+
+    @FXML
+    private Button towerButton5;
+
+    @FXML
+    void onConfirm(ActionEvent event) {
+
+    }
+
+}
