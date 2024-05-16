bb6be5e20d17adb58aa49e938d04e5483b390dbf
Finn <fbr42@uclive.ac.nz>
2024-05-16 12:44:11 +1200

    inventory and shop screen fmxl files and controllers

---
+++ src/main/java/seng201/team0/gui/InventoryScreenController.java
@@ -0,0 +1,53 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class InventoryScreenController {
+
+    @FXML
+    private Button inventorySlot1;
+
+    @FXML
+    private Button inventorySlot10;
+
+    @FXML
+    private Button inventorySlot2;
+
+    @FXML
+    private Button inventorySlot3;
+
+    @FXML
+    private Button inventorySlot4;
+
+    @FXML
+    private Button inventorySlot5;
+
+    @FXML
+    private Button inventorySlot6;
+
+    @FXML
+    private Button inventorySlot7;
+
+    @FXML
+    private Button inventorySlot8;
+
+    @FXML
+    private Button inventorySlot9;
+
+    @FXML
+    private Button inventoryToShopButton;
+
+    @FXML
+    private Button useHealButton;
+
+    @FXML
+    private Button useReviveButton;
+
+    @FXML
+    private Button useUpgradeButton;
+
+}
+
+
+
+
---
+++ src/main/java/seng201/team0/gui/ShopScreenController.java
@@ -0,0 +1,53 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ShopScreenController {
+
+    @FXML
+    private Button buyButton;
+
+    @FXML
+    private Button coalTower1Button;
+
+    @FXML
+    private Button coalTower2Button;
+
+    @FXML
+    private Button coalTower3Button;
+
+    @FXML
+    private Button emeraldTower1Button;
+
+    @FXML
+    private Button emeraldTower2Button;
+
+    @FXML
+    private Button emeraldTower3Button;
+
+    @FXML
+    private Button goldTower1Button;
+
+    @FXML
+    private Button goldTower2Button;
+
+    @FXML
+    private Button goldTower3Button;
+
+    @FXML
+    private Button sellButton;
+
+    @FXML
+    private Button shopToInventoryButton;
+
+    @FXML
+    private Button towerHealShopButton;
+
+    @FXML
+    private Button towerReviveShopButton;
+
+    @FXML
+    private Button towerUpgradeShopButton;
+
+}
