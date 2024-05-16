f6cf83bf8f2a6d98c9c2264a45399a0338a66d82
Sam Beattie <kiwisalmon2005@gmail.com>
2024-05-17 11:53:11 +1200

    round one inventory screen tower select working, multiple towers still selectable

--- src/main/java/seng201/team0/gui/RoundOneInventoryScreenController.java
+++ src/main/java/seng201/team0/gui/RoundOneInventoryScreenController.java
@@ -7,7 +7,9 @@ import seng201.team0.GameManager;
 import seng201.team0.models.Tower;
 import seng201.team0.services.RoundOneInventoryService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RoundOneInventoryScreenController {
     @FXML Button heavyCoalButton;
@@ -24,7 +26,7 @@ public class RoundOneInventoryScreenController {
     @FXML Label towerTypeLabel;
     @FXML Label towerReloadLabel;
     private final Tower[] selectedTowers = new Tower[3];
-    RoundOneInventoryService roundOneInventory;
+    private int selectedTowerIndex = -1;
 
     GameManager roundOneInventoryScreenGameManager;
 
@@ -33,12 +35,45 @@ public class RoundOneInventoryScreenController {
     }
     public void initialize() {
         List<Button> towerButtons = List.of(heavyCoalButton,lightCoalButton,heavyIronButton,lightIronButton,heavyGoldButton,lightGoldButton);
-        List<Button> selectedTowers = List.of(selectedTowerButtonOne,selectedTowerButtonTwo,selectedTowerButtonThree);
+        List<Button> selectedTowerButtons = List.of(selectedTowerButtonOne,selectedTowerButtonTwo,selectedTowerButtonThree);
         RoundOneInventoryService roundOneInventory = new RoundOneInventoryService(roundOneInventoryScreenGameManager);
+        List<Tower> towers = roundOneInventory.getTowerList();
+
+        for (int i = 0; i < towerButtons.size(); i++) {
+            int finalI = i;
+            towerButtons.get(i).setOnAction(event -> {
+                updateDisplayedStats(towers.get(finalI));
+                selectedTowerIndex = finalI;
+                towerButtons.forEach(button -> {
+                    if (button == towerButtons.get(finalI)) {
+                        button.setStyle("-fx-background-color: #b3b3b3; -fx-background-radius: 5;");
+                    } else {
+                        button.setStyle("");
+                    }
+                });
+            });
+        }
+        for (int i = 0; i < selectedTowerButtons.size(); i++) {
+            int finalI = i; //TODO stop multiple of the same tower being added
+            selectedTowerButtons.get(i).setOnAction(event -> {
+                if (selectedTowerIndex != -1) {
+                    selectedTowerButtons.get(finalI).setText(towers.get(selectedTowerIndex).getTowerName());
+                    selectedTowers[finalI] = towers.get(selectedTowerIndex);
+                }
+            });
+        }
+
+    }
+    private void updateDisplayedStats(Tower tower) {
+        towerNameLabel.setText("Name: "+tower.getTowerName());
+        towerHealthLabel.setText("Health: "+tower.getHealth());
+        towerTypeLabel.setText("Resource Fill Type: "+tower.getFillType());
+        towerReloadLabel.setText("Reload Speed: "+tower.getReloadSpeed());
 
     }
     @FXML
     private void onConfirm() {
+        // TODO save selected towers
         roundOneInventoryScreenGameManager.closeRoundOneInventoryScreen();
     }
 }
--- src/main/java/seng201/team0/services/RoundOneInventoryService.java
+++ src/main/java/seng201/team0/services/RoundOneInventoryService.java
@@ -3,14 +3,25 @@ package seng201.team0.services;
 import seng201.team0.GameManager;
 import seng201.team0.models.Tower;
 
+import java.util.List;
+
 public class RoundOneInventoryService {
+    Tower HeavyCoal;
+    Tower LightCoal;
+    Tower HeavyIron;
+    Tower LightIron;
+    Tower HeavyGold;
+    Tower LightGold;
     public RoundOneInventoryService(GameManager roundOneServiceGameManager){
         double currDifficulty = roundOneServiceGameManager.getDifficulty();
-        Tower HeavyCoal = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Coal", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Coal", currDifficulty);
-        Tower LightCoal = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Coal", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Coal", currDifficulty);
-        Tower HeavyIron = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Iron", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Iron", currDifficulty);
-        Tower LightIron = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Iron", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Iron", currDifficulty);
-        Tower HeavyGold = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Gold", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Gold", currDifficulty);
-        Tower LightGold = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Gold", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Gold", currDifficulty);
+        HeavyCoal = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Coal", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Coal", currDifficulty);
+        LightCoal = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Coal", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Coal", currDifficulty);
+        HeavyIron = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Iron", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Iron", currDifficulty);
+        LightIron = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Iron", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Iron", currDifficulty);
+        HeavyGold = new Tower(TowerGenerator.heavyTowerHealthGenerator(),true, "Gold", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Gold", currDifficulty);
+        LightGold = new Tower(TowerGenerator.lightTowerHealthGenerator(),true, "Gold", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Gold", currDifficulty);
+    }
+    public List<Tower> getTowerList(){
+        return List.of(HeavyCoal,LightCoal,HeavyIron,LightIron,HeavyGold,LightGold);
     }
 }
