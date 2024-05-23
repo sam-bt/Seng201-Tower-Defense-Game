87e69ada9218cc27bb0b5c87b726c88aa9de89aa
Samuel Beattie <sbe119@cs23129fw.canterbury.ac.nz>
2024-05-23 13:44:56 +1200

    started working on random events

--- src/main/java/seng201/team0/models/RandomEvent.java
+++ src/main/java/seng201/team0/models/RandomEvent.java
@@ -3,8 +3,12 @@ package seng201.team0.models;
 import seng201.team0.services.RandomEventGenerator;
 
 public class RandomEvent { // return a message and action based on generated event
+    RandomEventGenerator randomGenerator;
     public void generateRandomEvent(double difficulty) { // todo make bad events more likely as the difficulty increases
         int badEventLikelihood = (int) Math.round(difficulty);
-        RandomEventGenerator randomGenerator = new RandomEventGenerator(badEventLikelihood);
+        randomGenerator = new RandomEventGenerator(badEventLikelihood);
+    }
+    public void generateEvent(){
+        randomGenerator.generateEvent();
     }
 }
