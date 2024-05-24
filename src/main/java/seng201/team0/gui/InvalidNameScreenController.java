18dfd46fcda5e67d70ca66778fadb3813568b718
fbr42 <fbr42@uclive.ac.nz>
2024-05-24 19:44:20 +1200

    java docs for InvalidNameScreenController

--- src/main/java/seng201/team0/gui/InvalidNameScreenController.java
+++ src/main/java/seng201/team0/gui/InvalidNameScreenController.java
@@ -4,17 +4,32 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import seng201.team0.GameManager;
 
+/**
+ * Controller class for the InvalidNameScreen.
+ * Handles actions and interactions on the screen where an invalid name is entered.
+ */
 public class InvalidNameScreenController {
 
-    private Button returnToMainMenuButton;
 
+    /**
+     * Instance of the game manager associated with the screen for managing game state.
+     */
     GameManager invalidNameGameManager;
+
     @FXML
     private Button nextRoundButton;
+
+    /**
+     * Constructor for the InvalidNameScreenController class.
+     * @param tempInvalidNameGameManager The game manager instance associated with the screen.
+     */
     public InvalidNameScreenController(GameManager tempInvalidNameGameManager){
         invalidNameGameManager = tempInvalidNameGameManager;
     }
 
+    /**
+     * Closes the screen invalid name screen, Launches setup screen.
+     */
     @FXML
     private void invalidNameOnConfirm() {
         invalidNameGameManager.closeInvalidNameScreen();
