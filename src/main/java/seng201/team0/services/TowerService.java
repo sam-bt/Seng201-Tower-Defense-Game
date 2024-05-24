cdf01da5f1a723c156e1b0dd71b7629d0f62a260
fbr42 <fbr42@uclive.ac.nz>
2024-05-24 22:36:06 +1200

    java docs for TowerService

--- src/main/java/seng201/team0/services/TowerService.java
+++ src/main/java/seng201/team0/services/TowerService.java
@@ -4,8 +4,22 @@ import seng201.team0.models.Tower;
 
 import java.util.*;
 
+/**
+ * Class for tower-related operations.
+ */
 public class TowerService {
+
+    /**
+     * Random object used for generating random numbers.
+     */
     private static final Random rnd = new Random();
+
+    /**
+     * Checks if all towers in the given array are selected.
+     *
+     * @param selectedTowers The array of selected towers.
+     * @return True if all towers are selected, otherwise false.
+     */
     public static boolean areAllTowersSelected(Tower[] selectedTowers) {
         for (Tower tower : selectedTowers) {
             if (tower == null) {
@@ -14,6 +28,14 @@ public class TowerService {
         }
         return true;
     }
+
+    /**
+     * Checks if the given tower is already selected among the selected towers.
+     *
+     * @param selectedTowers The array of selected towers.
+     * @param tower The tower to check.
+     * @return True if the tower is already selected, otherwise false.
+     */
     public static boolean isTowerAlreadySelected(Tower[] selectedTowers, Tower tower) {
         for (Tower selectedTower: selectedTowers){
             if (selectedTower == null) {continue;}
@@ -24,6 +46,12 @@ public class TowerService {
         return false;
     }
 
+    /**
+     * Checks if all types of round one towers are selected among the selected towers.
+     *
+     * @param selectedTowers The array of selected towers.
+     * @return True if all round one tower types are selected, otherwise false.
+     */
     public static boolean areAllRoundOneTowersTypesSelected(Tower[] selectedTowers) {
         HashSet<String> roundOneTowers = new HashSet<>(Arrays.asList("Coal", "Iron", "Gold"));
         for (Tower tower : selectedTowers) {
@@ -32,10 +60,23 @@ public class TowerService {
         }
         return roundOneTowers.isEmpty();
     }
+
+    /**
+     * Determines if a tower should break based on its break chance.
+     *
+     * @param tower The tower to check.
+     * @return True if the tower should break, otherwise false.
+     */
     public static boolean shouldTowerBreak(Tower tower){
         int likelihood = rnd.nextInt(101);
         return tower.getBreakChance() > likelihood;
     }
+
+    /**
+     * Breaks towers in the given list if they are owned and should break.
+     *
+     * @param towerList The list of towers to check and break.
+     */
     public static void breakTowers(List<Tower> towerList){
         for (Tower tower: towerList) {
             if (tower.getOwned() && shouldTowerBreak(tower)) {
