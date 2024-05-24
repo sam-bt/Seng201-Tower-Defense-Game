bc0dc9451e93c93139ae1a8b6db6ef3a8679d1ab
fbr42 <fbr42@uclive.ac.nz>
2024-05-24 19:22:33 +1200

    java docs for InventoryService

--- src/main/java/seng201/team0/services/InventoryService.java
+++ src/main/java/seng201/team0/services/InventoryService.java
@@ -5,21 +5,75 @@ import seng201.team0.models.Tower;
 
 import java.util.List;
 
+/**
+ * Service for managing the inventory in the game.
+ * This includes tracking and initializing various types of towers,
+ * and the available revives, heals, and upgrades.
+ */
 public class InventoryService {
-    Tower HeavyCoal;
-    Tower LightCoal;
-    Tower HeavyIron;
-    Tower LightIron;
-    Tower HeavyGold;
-    Tower LightGold;
-    Tower HeavyGem;
-    Tower LightGem;
-    public int availableRevives;
-    public int availableHeals;
-    public int availableUpgrades;
-    public InventoryService(GameManager inventoryServiceGameManager){
-        double currDifficulty = inventoryServiceGameManager.getDifficulty();
+    /**
+     * Heavy coal tower object.
+     */
+    private Tower HeavyCoal;
 
+    /**
+     * Light coal tower object.
+     */
+    private Tower LightCoal;
+
+    /**
+     * Heavy iron tower object.
+     */
+    private Tower HeavyIron;
+
+    /**
+     * Light iron tower object.
+     */
+    private Tower LightIron;
+
+    /**
+     * Heavy gold tower object.
+     */
+    private Tower HeavyGold;
+
+    /**
+     * Light gold tower object.
+     */
+    private Tower LightGold;
+
+    /**
+     * Heavy gem tower object.
+     */
+    private Tower HeavyGem;
+
+    /**
+     * Light gem tower object.
+     */
+    private Tower LightGem;
+
+    /**
+     * The number of available revives in the inventory.
+     */
+    public int availableRevives;
+
+    /**
+     * The number of available heals in the inventory.
+     */
+    public int availableHeals;
+
+    /**
+     * The number of available upgrades in the inventory.
+     */
+    public int availableUpgrades;
+
+    /**
+     * Constructor for InventoryService.
+     * Initializes the inventory based on whether it's the first time entering the inventory or not.
+     *
+     * @param inventoryServiceGameManager the game manager instance used to retrieve game-related information
+     */
+    public InventoryService(GameManager inventoryServiceGameManager) {
+        double currDifficulty = inventoryServiceGameManager.getDifficulty();
 
         if (!inventoryServiceGameManager.isNotFirstTimeInInventory()) {
             inventoryServiceGameManager.setNotFirstTimeInInventory(true);
@@ -32,8 +86,26 @@ public class InventoryService {
             LightIron = inventoryServiceGameManager.getRoundOneTowerList().get(3);
             HeavyGold = inventoryServiceGameManager.getRoundOneTowerList().get(4);
             LightGold = inventoryServiceGameManager.getRoundOneTowerList().get(5);
-            HeavyGem = new Tower(TowerGenerator.heavyTowerHealthGenerator(),false, "Gem", TowerGenerator.heavyTowerFillAmountGenerator(),  TowerGenerator.heavyTowerReloadSpeedGenerator(), "Heavy Gem", currDifficulty, TowerGenerator.heavyTowerPriceGenerator(currDifficulty));
-            LightGem = new Tower(TowerGenerator.lightTowerHealthGenerator(),false, "Gem", TowerGenerator.lightTowerFillAmountGenerator(),  TowerGenerator.lightTowerReloadSpeedGenerator(), "Light Gem", currDifficulty, TowerGenerator.lightTowerPriceGenerator(currDifficulty));
+            HeavyGem = new Tower(
+                    TowerGenerator.heavyTowerHealthGenerator(),
+                    false,
+                    "Gem",
+                    TowerGenerator.heavyTowerFillAmountGenerator(),
+                    TowerGenerator.heavyTowerReloadSpeedGenerator(),
+                    "Heavy Gem",
+                    currDifficulty,
+                    TowerGenerator.heavyTowerPriceGenerator(currDifficulty)
+            );
+            LightGem = new Tower(
+                    TowerGenerator.lightTowerHealthGenerator(),
+                    false,
+                    "Gem",
+                    TowerGenerator.lightTowerFillAmountGenerator(),
+                    TowerGenerator.lightTowerReloadSpeedGenerator(),
+                    "Light Gem",
+                    currDifficulty,
+                    TowerGenerator.lightTowerPriceGenerator(currDifficulty)
+            );
             List<Tower> towerList = List.of(HeavyCoal, LightCoal, HeavyIron, LightIron, HeavyGold, LightGold, HeavyGem, LightGem);
             inventoryServiceGameManager.setGenericRoundTowerList(towerList);
         } else {
@@ -50,7 +122,13 @@ public class InventoryService {
             LightGem = inventoryServiceGameManager.getGenericRoundTowerList().get(7);
         }
     }
-    public List<Tower> getTowerList(){
-        return List.of(HeavyCoal,LightCoal,HeavyIron,LightIron,HeavyGold,LightGold,HeavyGem,LightGem);
+
+    /**
+     * Returns the list of towers in the inventory.
+     *
+     * @return a list containing all the towers
+     */
+    public List<Tower> getTowerList() {
+        return List.of(HeavyCoal, LightCoal, HeavyIron, LightIron, HeavyGold, LightGold, HeavyGem, LightGem);
     }
 }
