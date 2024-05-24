a6c2e9df9c8bc675426180e5a3c4b04d6235fab1
fbr42 <fbr42@uclive.ac.nz>
2024-05-24 21:52:03 +1200

    java docs for CartService

--- src/main/java/seng201/team0/services/CartService.java
+++ src/main/java/seng201/team0/services/CartService.java
@@ -4,7 +4,15 @@ import seng201.team0.models.Cart;
 
 import java.util.List;
 
+/**
+ * Provides methods for carts in the game.
+ */
 public class CartService {
+    /**
+     * Checks if all carts except the bonus cart are full.
+     * @param cartList The list of carts to be checked.
+     * @return {@code true} if all carts except the bonus cart are full, {@code false} otherwise.
+     */
     public static boolean areAllCartsFull(List<Cart> cartList) { //checks all but the bonus cart
         for (Cart cart: cartList) {
             if (cart.isFull() == false && cart.getCartName() != "Bonus") {
