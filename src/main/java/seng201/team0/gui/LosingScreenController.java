5e00dce1725fcaadb1e5ff9a6fda05b6a0048b48
fbr42 <fbr42@uclive.ac.nz>
2024-05-24 19:46:49 +1200

    java docs for LosingScreenController

--- src/main/java/seng201/team0/gui/LosingScreenController.java
+++ src/main/java/seng201/team0/gui/LosingScreenController.java
@@ -5,25 +5,48 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import seng201.team0.GameManager;
 
+/**
+ * Controller class for the LosingScreen.fxml file.
+ * Handles actions and interactions on the screen displayed when the player loses the game.
+ */
 public class LosingScreenController {
+
     @FXML
     private Label roundsCompletedLabel;
+
     @FXML
     private Label difficultyLabel;
+
     @FXML
     private Label pointsLabel;
+
+    /**
+     * Game manager for the losing screen.
+     */
     GameManager losingScreenGameManager;
+
+    /**
+     * Constructor for the LosingScreenController class.
+     * @param tempLosingScreenGameManager The game manager instance associated with the losing screen.
+     */
     public LosingScreenController(GameManager tempLosingScreenGameManager) {
         losingScreenGameManager = tempLosingScreenGameManager;
+    }
 
-    }
+    /**
+     * Initializes the losing screen with relevant information such as rounds completed, points earned, and difficulty level.
+     */
     public void initialize() {
-        roundsCompletedLabel.setText("You completed "+(losingScreenGameManager.getCurrRound()-2)+"/"+losingScreenGameManager.getRounds()+" Rounds!");
-        pointsLabel.setText("You had "+losingScreenGameManager.getPoints()+" Points!");
-        difficultyLabel.setText("You lost on difficulty "+losingScreenGameManager.getDifficulty()+"!");
+        roundsCompletedLabel.setText("You completed " + (losingScreenGameManager.getCurrRound() - 2) + "/" + losingScreenGameManager.getRounds() + " Rounds!");
+        pointsLabel.setText("You had " + losingScreenGameManager.getPoints() + " Points!");
+        difficultyLabel.setText("You lost on difficulty " + losingScreenGameManager.getDifficulty() + "!");
     }
-    public void onQuit(){
+
+    /**
+     * Event handler for quitting the game.
+     * Exits the application when called.
+     */
+    public void onQuit() {
         System.exit(0);
     }
-
 }
